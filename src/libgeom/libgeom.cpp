1d251f40ca139c001b1c4dc917e74923ec8c693a
isigurohatune <39hatune.1925@gmail.com>
2021-07-18 23:37:50 +0900

    最後の一つが分からなかったのですが、それ以外はテストに通るようにしました。

--- src/libgeom/libgeom.cpp
+++ src/libgeom/libgeom.cpp
@@ -8,42 +8,40 @@
 void copy_vector4_array(float* dst, const float* src, int num)
 {
 #if 1
+
+	for (int i = 0; i < num * 4; i += 4)
+	{
+
+		__m128 pd = _mm_load_ps(src + i);
+		_mm_store_ps( dst + i, pd);
+
+	}
 	// ToDo: SIMD計算を使って実装して下さい
 #else
 	float* pd = dst;
 	const float* ps = src;
-
-	for (int i = 0; i < num; i++) {
-		pd[0] = ps[0];
-		pd[1] = ps[1];
-		pd[2] = ps[2];
-		pd[3] = ps[3];
-
-		pd += 4;
-		ps += 4;
-	}
 #endif
+	
 }
 
 // 4つのfloat が num個の配列 src0, src1を足してdstに出力
 void add_vector4_array(float* dst, const float* src0, const float* src1, int num)
 {
 #if 1
+	for (int i = 0; i < num * 4; i += 4) 
+	{
+		__m128 ps0 = _mm_load_ps(src0 + i);
+		__m128 ps1 = _mm_load_ps(src1 + i);
+
+		__m128 ps = _mm_add_ps(ps0, ps1);
+		_mm_store_ps(dst + i, ps);
+	}
+
 	// ToDo: SIMD計算を使って実装して下さい
 #else
 	float* pd = dst;
 	const float* ps0 = src0;
 	const float* ps1 = src1;
-
-	for (int i = 0; i < num; i++) {
-		pd[0] = ps0[0] + ps1[0];
-		pd[1] = ps0[1] + ps1[1];
-		pd[2] = ps0[2] + ps1[2];
-		pd[3] = ps0[3] + ps1[3];
-
-		pd += 4;
-		ps0 += 4;
-		ps1 += 4;
 	}
 #endif
 }
@@ -52,19 +50,20 @@ void add_vector4_array(float* dst, const float* src0, const float* src1, int num
 void apply_matrix_vector4_array(float* dst, const float* src, const float* matrix, int num)
 {
 #if 1
+	for (int i = 0; i < num * 4; i += 4) 
+	{
+		__m128 ps = _mm_load_ps(src + i);
+		__m128 mat = _mm_load_ps(matrix + i);
+
+		__m128 a = _mm_mul_ps(ps, mat);
+		_mm_store_ps(dst + i,a);
+
+
+	}
 	// ToDo: SIMD計算を使って実装して下さい
 #else
 	float* pd = dst;
 	const float* ps = src;
-
-	for (int i = 0; i < num; i++) {
-		pd[0] = matrix[4*0+0]*ps[0] + matrix[4*0+1]*ps[1] + matrix[4*0+2]*ps[2] + matrix[4*0+3]*ps[3];
-		pd[1] = matrix[4*1+0]*ps[0] + matrix[4*1+1]*ps[1] + matrix[4*1+2]*ps[2] + matrix[4*1+3]*ps[3];
-		pd[2] = matrix[4*2+0]*ps[0] + matrix[4*2+1]*ps[1] + matrix[4*2+2]*ps[2] + matrix[4*2+3]*ps[3];
-		pd[3] = matrix[4*3+0]*ps[0] + matrix[4*3+1]*ps[1] + matrix[4*3+2]*ps[2] + matrix[4*3+3]*ps[3];
-
-		pd += 4;
-		ps += 4;
 	}
 #endif
 }
